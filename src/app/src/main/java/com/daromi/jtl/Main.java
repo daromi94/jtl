ef09e826d16ed59be12ebadef64427c8b761dc8f
daromi94 <davromero07@gmail.com>
2025-11-29 18:19:57 -0500

    feat: :sparkles: add thread limit test

--- src/app/src/main/java/com/daromi/jtl/Main.java
+++ src/app/src/main/java/com/daromi/jtl/Main.java
@@ -1,8 +1,25 @@
 package com.daromi.jtl;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.LockSupport;
+
 final class Main {
 
   public static void main(final String[] args) {
-    System.out.println("Hello, World!");
+    final var threadCount = new AtomicInteger(1);
+
+    try {
+      while (true) {
+        Thread.ofPlatform()
+            .start(
+                () -> {
+                  threadCount.incrementAndGet();
+                  LockSupport.park();
+                });
+      }
+    } catch (final OutOfMemoryError e) {
+      System.err.printf("Reached thread limit %d\n", threadCount.get());
+      e.printStackTrace();
+    }
   }
 }
