aea26ccd54f94b39985dcd2d3ea5a79ccf8bda8a
daromi94 <davromero07@gmail.com>
2025-11-30 17:18:02 -0500

    refactor: :recycle: switch to daemon threads and improve logging

--- src/app/src/main/java/com/daromi/jtl/Main.java
+++ src/app/src/main/java/com/daromi/jtl/Main.java
@@ -1,25 +1,33 @@
 package com.daromi.jtl;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.LockSupport;
 
 final class Main {
 
   public static void main(final String[] args) {
-    final var threadCount = new AtomicInteger(1);
+    var threadCount = 0;
 
     try {
       while (true) {
         Thread.ofPlatform()
-            .start(
-                () -> {
-                  threadCount.incrementAndGet();
-                  LockSupport.park();
-                });
+            // Standard "User Threads" prevent the JVM from exiting until they finish.
+            // "Daemon Threads" depend on the Main thread. If Main crashes (OOM),
+            // the JVM kills all Daemon threads instantly, preventing a zombie process.
+            .daemon()
+            // Puts the thread in a WAITING state, removing it from the OS scheduler.
+            // This ensures we consume RAM (Stack Memory) to hit the limit,
+            // but consume 0% CPU while waiting for that limit.
+            .start(LockSupport::park);
+
+        threadCount++;
+
+        if (threadCount % 500 == 0) {
+          System.out.printf("Spawned: %d threads...\n", threadCount);
+        }
       }
     } catch (final OutOfMemoryError e) {
-      System.err.printf("Reached thread limit: %d\n", threadCount.get());
-      e.printStackTrace();
+      System.err.printf("Reached Limit: %d threads\n", threadCount);
+      System.err.printf("JVM Error: %s\n", e.getMessage());
     }
   }
 }
